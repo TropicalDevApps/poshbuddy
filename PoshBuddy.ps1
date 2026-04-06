31aaea090505988556a1174368b4577f97db8357
Julio <julio@estudio.com>
2026-04-06 09:21:24 -0600

    chore: initial project structure and global state definition (v0.0.1-alpha.1)

---
+++ PoshBuddy.ps1
@@ -0,0 +1,7 @@
+# PoshBuddy v0.0.1-alpha.1
+$PoshBuddy = @{
+    Version = "0.0.1-alpha.1"
+    ThemesDir = Join-Path $HOME ".poshthemes"
+    Config = @{}
+}
+Write-Host "PoshBuddy Engine Initialized"
